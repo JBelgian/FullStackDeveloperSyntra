962da7cb141b1a189894cf19f82678bac79b7180
Jordy <jordy_hanno@hotmail.com>
2024-11-12 15:29:39 +0100

    reorder

---
+++ 5_JS_Notities/fetch.js
@@ -0,0 +1,10 @@
+const url = 'http://localhost:3000/posts/';
+const output = document.getElementById('output')
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    for (let i = 0; i < data.length; i++) {
+        output.innerHTML += `<li>${data[i].title} had ${data[i].views} views</li>`
+    }
+})
+.catch(e => console.log(e));
