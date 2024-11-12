2fde04dc0c68242a363286e087b58af691ebf884
Jordy <jordy_hanno@hotmail.com>
2024-11-12 15:58:34 +0100

    lest 12/11 eod

--- 5_JS_Notities/fetch.js
+++ 5_JS_Notities/fetch.js
@@ -1,10 +1,10 @@
-const url = 'http://localhost:3000/posts/';
+const url = 'http://localhost:3000/leerlingen/';
 const output = document.getElementById('output')
 fetch(url)
 .then(res => res.json())
 .then(data => {
     for (let i = 0; i < data.length; i++) {
-        output.innerHTML += `<li>${data[i].title} ${data[i].views}</li>`
+        output.innerHTML += `<li>${data[i].firstName} ${data[i].lastName}</li>`
     }
 })
 .catch(e => console.log(e));
