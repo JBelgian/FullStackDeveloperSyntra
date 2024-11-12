abbfa846cc2ea4db05d5e441c033994ae7bbd10e
Jordy <jordy_hanno@hotmail.com>
2024-11-12 12:55:51 +0100

    les 12/11 middag

--- JS_Notities/fetch.js
+++ JS_Notities/fetch.js
@@ -2,7 +2,14 @@ const url = 'https://jsonplaceholder.typicode.com/todos/';
 
 fetch(url)
 .then(res => res.json())
-.then(data => console.log(data))
+.then(data => {
+    const output2 = document.getElementById('output')
+    for (let i = 0; i < data.length; i++) {
+        output2.innerHTML += `<li>${data[i].title}</li>`
+    }
+    return output2.innerHTML
+})
 .catch(e => console.log(e));
 
 // HTTP VERB (GET) (POST, DELETE, PUT)
+
