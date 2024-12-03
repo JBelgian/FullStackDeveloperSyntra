3778f3d19923b9ae791e57d99cc2c8ce8fb2f4bd
Jordy <jordy_hanno@hotmail.com>
2024-12-03 13:25:55 +0100

    vite app

--- 5_JS_Notities/my-vite-app/src/main.ts
+++ 5_JS_Notities/my-vite-app/src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import  _  from  'lodash'
+import  './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const  numbers:  number[] = [34, 12, 78, 3, 45, 21]
+const  output:  HTMLElement  =  document.getElementById('output')!
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// Sort ascending _.sortBy only sort ascending
+const  ascending:  number[] =  _.sortBy(numbers)
+output.innerHTML  +=  `<p>Ascending: ${ascending.join(', ')}</p>`
+
+// Sort descending
+const  descending:  number[] =  _.orderBy(numbers, [], ['desc'])
+output.innerHTML  +=  `<p>Descending: ${descending.join(', ')}</p>`
