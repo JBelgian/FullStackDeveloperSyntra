863f9d5b767baaf0ff9818c5d51f17f9c0673bde
Jordy <jordy_hanno@hotmail.com>
2024-12-11 13:11:40 +0100

    eod 10-12

--- 7_Angular/angular_oef/src/app/app.component.ts
+++ 7_Angular/angular_oef/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserComponent } from './user/user.component';
+import { UserComponent } from "./user/user.component";
 
 @Component({
   selector: 'app-root',
@@ -9,6 +9,5 @@ import { UserComponent } from './user/user.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Jordy';
-  xmas: boolean = false;
+  title = 'Jordy Oefening';
 }
